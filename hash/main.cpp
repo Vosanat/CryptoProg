840a0cc3d67beb6a42469b4b4b2e9d11d856cd48
Vosanat <stud@virtdeb.local>
2023-12-11 09:38:44 +0300

    add hex

--- hash/main.cpp
+++ hash/main.cpp
@@ -12,12 +12,12 @@ int main()
     std::ifstream file("text.txt");
     CryptoPP::SHA256 hash;
     byte digest[CryptoPP::SHA256::DIGESTSIZE];
-    FileSource(file, true, new HashFilter(hash, new ArraySink(digest, CryptoPP::SHA256::DIGESTSIZE)));
+    FileSource(file, true,  new HashFilter(hash, new CryptoPP::HexEncoder(new ArraySink(digest, CryptoPP::SHA256::DIGESTSIZE))));
     std::cout << "Hash: ";
     for (int i = 0; i < CryptoPP::SHA256::DIGESTSIZE; ++i) {
-        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(digest[i]);
+        std::cout << digest[i];
     }
-    std::cout << std::dec << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
